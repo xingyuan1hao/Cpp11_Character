f5278f1a102d12ad996e1782dba91a3b5b3fcc80
xingyuan1hao <ahhlin@163.om>
2019-06-01 22:29:25 +0800

    Update sprintf.cpp

--- std lib/sprintf.cpp
+++ std lib/sprintf.cpp
@@ -1065,5 +1065,17 @@ static int vsprintf(char *buf, const char *fmt, va_list args)
     return str - buf;
 }
 
+int sprintf(char *buf, const char *fmt, ...)
+{
+    va_list args;
+    int n;
+
+    va_start(args, fmt);
+    n = vsprintf(buf, fmt, args);
+    va_end(args);
+
+    return n;
+}
+
 /* ///////////////////////////////////////////////////////////////////////////// */
 
